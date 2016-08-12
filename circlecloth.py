290addb16d731c94f3fc1f542a056527e3663efa
Brijen Thananjeyan <brijenthananjeyan@gmail.com>
2016-08-11 21:34:23 -0700

    updated cloth

--- circlecloth.py
+++ circlecloth.py
@@ -50,27 +50,3 @@ class CircleCloth(Cloth):
                 else:
                     self.normalpts.remove(pt)
 
-    def pin_position(self, x, y):
-        """
-        Grab a position on the cloth and pin it in place.
-        """
-        for pt in self.pts:
-            if abs((pt.x - x) ** 2 + (pt.y - y) ** 2) < 1000:
-                pt.pinned = True
-                self.grabbed_pts.append(pt)
-
-    def unpin_position(self, x, y):
-        """
-        Let go of a grabbed position.
-        """
-        if abs((pt.x - x) ** 2 + (pt.y - y) ** 2) < 1000:
-            pt.pinned = False
-            self.grabbed_pts.remove(pt)
-
-    def tension(self, x, y, z=0):
-        """
-        Tug on the grabbed area in a direction.
-        """
-        for pt in self.grabbed_pts:
-            pt.px, pt.py, pt.pz = pt.x, pt.y, pt.z
-            pt.x, pt.y, pt.z = x + pt.x, y + pt.y, z + pt.z
--- cloth.py
+++ cloth.py
@@ -12,6 +12,7 @@ class Cloth(object):
         """
         self.mouse = mouse
         self.pts = []
+        self.grabbed_pts = []
         for i in range(height):
             for j in range(width):
                 pt = Point(mouse, 50 + dx * j, 50 + dy * i)
@@ -38,3 +39,28 @@ class Cloth(object):
         for pt in self.pts:
             if pt.constraints == []:
                 self.pts.remove(pt)
+
+    def pin_position(self, x, y):
+        """
+        Grab a position on the cloth and pin it in place.
+        """
+        for pt in self.pts:
+            if abs((pt.x - x) ** 2 + (pt.y - y) ** 2) < 1000:
+                pt.pinned = True
+                self.grabbed_pts.append(pt)
+
+    def unpin_position(self, x, y):
+        """
+        Let go of a grabbed position.
+        """
+        if abs((pt.x - x) ** 2 + (pt.y - y) ** 2) < 1000:
+            pt.pinned = False
+            self.grabbed_pts.remove(pt)
+
+    def tension(self, x, y, z=0):
+        """
+        Tug on the grabbed area in a direction.
+        """
+        for pt in self.grabbed_pts:
+            pt.px, pt.py, pt.pz = pt.x, pt.y, pt.z
+            pt.x, pt.y, pt.z = x + pt.x, y + pt.y, z + pt.z
