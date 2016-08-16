3bb9831ac2d09adda18d5fababd829f7988d331d
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2016-08-16 14:21:26 -0700

    interpolation code

--- registration.py
+++ registration.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys, pickle
 from math import sqrt
 from shape_tracer import *
+from scipy.interpolate import interp1d
 
 def get_basis(corners):
     v1 = np.array(corners[1]) - np.array(corners[0])
@@ -19,20 +20,37 @@ def get_scale(corners, px_distance=500):
 def transform_and_project_point(transform, scale, pt, corners, offset=(50,50)):
     return np.ravel(np.matrix(transform) * np.matrix((np.array(pt)  - np.array(corners[0]))).T * scale)[:2] + np.array(offset)
 
-def get_shape_fn(corners, pts):
+def get_shape_fn(corners, pts, interpolate=False):
     scale = get_scale(corners)
     basis = get_basis(corners)
     pxpts = []
     for pt in pts:
         pxpts.append(transform_and_project_point(basis, scale, pt, corners).tolist())
-    pxpts = np.matrix(pxpts)
-    return lambda x, y: np.min(np.linalg.norm(np.matrix(np.tile(np.array((x, y)), (len(pts), 1))) - pxpts, axis=1)) < 10
+    if interpolate:
+        pxpts = interpolation(np.array(pxpts), 10).tolist()
+    return lambda x, y: np.min(np.linalg.norm(np.matrix(np.tile(np.array((x, y)), (len(pxpts), 1))) - pxpts, axis=1)) < 10
+
+def interpolation(arr, factor):
+    """
+    Given a matrix of x,y coordinates, output a linearly interpolated matrix of coordinates with factor * arr.shape[0] points.
+    """
+    x = arr[:, 0]
+    y = arr[:, 1]
+    t = np.linspace(0,x.shape[0],num=x.shape[0])
+    to_expand = [x, y]
+    for i in range(len(to_expand)):
+        spl = interp1d(t, np.ravel(to_expand[i]))
+        to_expand[i] = spl(np.linspace(0,len(t), len(t)*factor))
+    new_matrix = np.matrix(np.r_[0:len(t):1.0/factor])
+    for i in to_expand:
+        new_matrix = np.concatenate((new_matrix, np.matrix(i)), axis = 0)
+    return new_matrix.T[:,1:]
 
 if __name__ == "__main__":
-    corners = [(1, 1, 0), (2, 1, 0), (1, 2, 0), (2, 2, 0)]
-    fn = get_shape_fn(corners, [[2, 0, 0], [2,1,0], [2,2,0]])
+    # corners = [(1, 1, 0), (2, 1, 0), (1, 2, 0), (2, 2, 0)]
+    # fn = get_shape_fn(corners, [[2, 0, 0], [2,1,0], [2,2,0]])
 
-    corners = load_robot_points()[:4,:]
-    pts = load_robot_points()[4:,:]
+    corners = load_robot_points()
+    pts = load_robot_points("gauze_pts2.p")
 
-    shape_fn = get_shape_fn(corners, pts)
+    shape_fn = get_shape_fn(corners, pts, True)
--- simulation.py
+++ simulation.py
@@ -113,10 +113,9 @@ class Simulation(object):
 if __name__ == "__main__":
     shape_fn = lambda x, y: abs((x - 300) **2 + (y - 300) ** 2 - 150 **2) < 2000
 
-    corners = load_robot_points()
-    pts = load_robot_points("gauze_pts2.p")
-
-    shape_fn = get_shape_fn(corners, pts)
+    # corners = load_robot_points()
+    # pts = load_robot_points("gauze_pts2.p")
+    # shape_fn = get_shape_fn(corners, pts, True)
 
     mouse = Mouse(down=True)
     cloth = ShapeCloth(shape_fn, mouse)
