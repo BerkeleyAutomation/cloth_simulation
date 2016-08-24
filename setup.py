9b7f8b6357cb0982b7ad3484f1e6dd76381ff660
Brijen Thananjeyan <brijenthananjeyan@gmail.com>
2016-08-23 17:46:25 -0700

    cythonize tensioner

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-files = "constraint.pyx", "point.pyx", "cloth.pyx", "shapecloth.pyx", "circlecloth.pyx", "scorer.pyx", "mouse.pyx"
+files = "constraint.pyx", "point.pyx", "cloth.pyx", "shapecloth.pyx", "circlecloth.pyx", "scorer.pyx", "mouse.pyx", "tensioner.pyx"
 
 for file in files:
 	setup(
